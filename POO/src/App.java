e2d25f155744533407d3dd910ce5cb1ffc464b0e
Marcos Antônio <marcosjunior.me.009@gmail.com>
2024-04-26 22:39:58 -0300

    feat: finalizando o código

---
+++ POO/src/App.java
@@ -0,0 +1,9 @@
+import service.imp.Iphone;
+
+public class App {
+
+    public static void main(String[] args) {
+       Iphone celular = new Iphone();
+       System.out.println(celular);
+    }
+}
